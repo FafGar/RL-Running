27a5a48ff78d08b3fc7593d74c3b916bb56f6163
Asher Dykstra <asherdykstra@gmail.com>
2023-07-31 16:44:16 -0600

    Reworkin nueral net inot a true Q-learning situation

--- Assets/DroneMover.cs
+++ Assets/DroneMover.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 
 public class DroneMover : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject target;
 
     public WheelCollider rightWheel;
     public WheelCollider leftWheel;
 
 
-    enum wheelstate:int{FORWARD=1,BACKWARD=-1,OFF=0}
+    enum wheelstate:int{FORWARD=100,BACKWARD=-100,OFF=0}
     wheelstate rightState;
     wheelstate leftState;
     Vector3 oldPos;
+    Vector3 startPos;
+    float moveTimer;
 
     Rigidbody body;
 
+    NeuralNet RLClient;
+    float [] inputs;
 
-    bool xAxisOkay(){
+
+    int xAxisOkay(){
         if(transform.rotation.eulerAngles.x > -15f && 
-            transform.rotation.eulerAngles.x < 15f) return true;
-        return false;
+            transform.rotation.eulerAngles.x < 15f) return 2;
+        return -3;
     }
-    bool zAxisOkay(){
+    int zAxisOkay(){
         if(transform.rotation.eulerAngles.z > -15f && 
-            transform.rotation.eulerAngles.z < 15f) return true;
-        return false;
+            transform.rotation.eulerAngles.z < 15f) return 2;
+        return -3;
     }
 
     int directionCheckRight(){
@@ -38,7 +42,7 @@ public class DroneMover : MonoBehaviour
         sightLine.Normalize();
         if(sightLine.x - targetLine.x < 0.3f) return 0;
         else if (sightLine.x - targetLine.x < -0.3f) return -1; //negative score if need to turn right
-        return 2; //reward if in the right area
+        return 3; //reward if in the right area
     }
 
     int directionCheckLeft(){
@@ -49,15 +53,23 @@ public class DroneMover : MonoBehaviour
         sightLine.Normalize();
         if(sightLine.x - targetLine.x < 0.3f) return -1; //negative score if need to turn left
         else if (sightLine.x - targetLine.x < -0.3f) return 0; 
-        return 2; //reward if in the right area
+        return 3; //reward if in the right area
     }
 
     int distanceCheck(){
         float dist = (transform.position-target.transform.position).magnitude;
         float oldDist = (oldPos-target.transform.position).magnitude;
-        if(oldDist - dist > 0.05) return 1; //Reward on move forward
-        if(oldDist - dist > 0 && oldDist - dist < 0.05) return -5; // mild penalty for staying still
-        return -10; //BIG penalty for moving backwards
+        oldPos = transform.position;
+        
+        if(oldDist - dist  > -0.0005 && oldDist - dist < 0.0005) {
+            // Debug.Log(moveTimer);
+            moveTimer += Time.deltaTime;
+            return -3; // mild penalty for staying still
+        }else if(oldDist - dist > 0.0005) {
+            moveTimer = 0;
+            return 5; //Reward on move forward
+        }
+        return -5; //BIG penalty for moving backwards
     }
 
 
@@ -66,31 +78,101 @@ public class DroneMover : MonoBehaviour
     {
         leftState = wheelstate.OFF;
         rightState = wheelstate.OFF;
+        moveTimer = 0;
         body=GetComponent<Rigidbody>();
+        startPos = transform.position;
+
+        this.RLClient = new NeuralNet();
+        this.RLClient.init(5,6,12);
+        this.inputs = new float[5];
+        this.inputs[4] = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.U)){
-            if(leftState==wheelstate.FORWARD) leftState=wheelstate.OFF;
-            else leftState=wheelstate.FORWARD;
+        this.inputs[0] = transform.rotation.x;
+        this.inputs[1] = transform.rotation.z;
+        this.inputs[2] = transform.position.x-target.transform.position.x;
+        this.inputs[3] = transform.position.z-target.transform.position.z;
+        
+
+        int RLOutput = RLClient.evaluate(inputs);
+        print(RLOutput);
+        int reward = 0;
+        if(this.inputs[4] == RLOutput && RLOutput !=6){
+            reward -= 5;
+        } else reward+=4;
+        this.inputs[4] = RLOutput;
+        switch (RLOutput)
+        {
+            case 0:
+                leftState=wheelstate.OFF;
+                break;
+            case 1:
+                leftState=wheelstate.FORWARD;
+                break;
+            case 2:
+                leftState=wheelstate.BACKWARD;
+                break;
+            case 3:
+                leftState=wheelstate.OFF;
+                break;
+            case 4:
+                leftState=wheelstate.FORWARD;
+                break;
+            case 5:
+                leftState=wheelstate.BACKWARD;
+                break;
+            case 6:
+                break;
         }
-        if(Input.GetKey(KeyCode.J)){
-            if(leftState==wheelstate.BACKWARD) leftState=wheelstate.OFF;
-            else leftState=wheelstate.BACKWARD;
-        }
-        if(Input.GetKey(KeyCode.I)){
-            if(rightState==wheelstate.FORWARD) rightState=wheelstate.OFF;
-            else rightState=wheelstate.FORWARD;
-        }
-        if(Input.GetKey(KeyCode.K)){
-            if(rightState==wheelstate.BACKWARD) rightState=wheelstate.OFF;
-            else rightState=wheelstate.BACKWARD;
-        }
+        // if(Input.GetKey(KeyCode.U)){
+        //     if(leftState==wheelstate.FORWARD) leftState=wheelstate.OFF;
+        //     else leftState=wheelstate.FORWARD;
+        // }
+        // if(Input.GetKey(KeyCode.J)){
+        //     if(leftState==wheelstate.BACKWARD) leftState=wheelstate.OFF;
+        //     else leftState=wheelstate.BACKWARD;
+        // }
+        // if(Input.GetKey(KeyCode.I)){
+        //     if(rightState==wheelstate.FORWARD) rightState=wheelstate.OFF;
+        //     else rightState=wheelstate.FORWARD;
+        // }
+        // if(Input.GetKey(KeyCode.K)){
+        //     if(rightState==wheelstate.BACKWARD) rightState=wheelstate.OFF;
+        //     else rightState=wheelstate.BACKWARD;
+        // }
         
         rightWheel.motorTorque = (int)rightState;
         leftWheel.motorTorque = (int)leftState;
-        body.AddForce(transform.up);
+        // if(leftState != 0){/
+        //     leftWheel.r
+        // }
+        body.AddForce(transform.up*40);
+        Debug.Log(rightState);
+
+
+
+        if(transform.position.y < 0 || moveTimer > 3.0){
+            Debug.Log("Resetting");
+            RLClient.train((int)(27-Mathf.Abs((target.transform.position-transform.position).magnitude)), 0.3);
+            transform.position = new Vector3(0,0.9f,0);
+            transform.rotation = Quaternion.identity;
+            moveTimer = 0;
+        }else if(Mathf.Abs(transform.rotation.x) > 70f || Mathf.Abs(transform.rotation.z) > 70f){
+            //Do Nothing
+        } 
+         else{
+            reward = xAxisOkay();
+            reward += zAxisOkay();
+            reward += directionCheckLeft();
+            reward += directionCheckRight();
+            reward += distanceCheck();
+            RLClient.train(reward, 0.1);
+        }
+
+
     }
 }
--- Assets/NeuralNet.cs
+++ Assets/NeuralNet.cs
@@ -4,68 +4,156 @@ using UnityEngine;
 
 public class NeuralNet
 {
-    double [] inputWeights;
-    double [,] layer1Weights;
-    double [,]layer2Weights;
-    double [] actionWeights;
+    struct neuron
+    {
+        public double [] weights;
+        public double bias;
+    }
+
+
+    neuron [] layer1Neurons;
+    neuron [] layer2Neurons;
+    neuron [] layer3Neurons;
+    neuron [] actionNeurons;
+
+    double[] layer1Vals;
+    double[] layer2Vals;
+    double[] layer3Vals;
+    double[] actionVals;
 
     int numInputs;
     int numOutputOptions;
+    int layerSize;
+    int maxReward;
+    double trainingCoefficient;
 
-    void init(int numInputs, int numOutputOptions){
+    public void init(int numInputs, int outputOptionsCount, int layerSize, int maxReward, double trainingCoefficient){
         this.numInputs = numInputs;
-        this.numOutputOptions = numOutputOptions;
+        this.numOutputOptions = outputOptionsCount+1; //adding room for a do nothing option
+        this.layerSize = layerSize;
+        this.maxReward = maxReward;
+        this.trainingCoefficient = trainingCoefficient;
 
-        inputWeights = new double[numInputs];
-        layer1Weights = new double[numInputs*4,numOutputOptions*4];
-        layer2Weights = new double[numOutputOptions*4, numOutputOptions];
-        actionWeights = new double[numOutputOptions];
+        layer1Neurons = new neuron[layerSize];
+        layer2Neurons = new neuron[layerSize];
+        layer3Neurons = new neuron[layerSize];
+        actionNeurons = new neuron[numOutputOptions];
 
-        for(int i=0;i<numInputs*4;i++){
-            if(i%4 == 0){
-                inputWeights[i/4] = Random.Range(1,50)/10d;
+        //Note for tryin to understand this later: Each layer has a number of neurons. Each neuron has their own set of weights with a # relative to the previous layer's # of neurons.
+        for(int i=0;i<layerSize;i++){
+            // layer1Neurons[i] = new neuron();
+            // layer2Neurons[i] = new neuron();
+            // layer3Neurons[i] = new neuron();
+            
+
+            layer1Neurons[i].bias = Random.Range(-50,50)/10d;
+            layer1Neurons[i].weights = new double[numInputs];
+
+            layer2Neurons[i].bias = Random.Range(-50,50)/10d;
+            layer2Neurons[i].weights = new double[layerSize];
+
+            layer3Neurons[i].bias = Random.Range(-50,50)/10d;
+            layer3Neurons[i].weights = new double[layerSize];
+
+            for(int j=0; j<numInputs; j++) layer1Neurons[i].weights[j] = Random.Range(1,50)/10d;
+            for(int j = 0;j<layerSize;j++){
+                layer2Neurons[i].weights[j] = Random.Range(1,50)/10d;
+                layer3Neurons[i].weights[j] = Random.Range(1,50)/10d;
             }
-            for(int j = 0; j<numOutputOptions*4;j++){
-                layer1Weights[i,j] = Random.Range(1,50)/10d;
-            }
+            
         }
 
-        for(int i=0;i<numOutputOptions*4;i++){
-            if(i%4 == 0){
-                actionWeights[i/4] = Random.Range(1,50)/10d;
-            }
-            for(int j = 0; j<numOutputOptions;j++){
-                layer1Weights[i,j] = Random.Range(1,50)/10d;
+        for(int i=0;i<numOutputOptions;i++){
+            actionNeurons[i] = new neuron();
+            actionNeurons[i].bias = Random.Range(1,50)/10d;
+            actionNeurons[i].weights = new double[layerSize];
+
+            for(int j = 0;j<layerSize;j++){
+                actionNeurons[i].weights[j] = Random.Range(1,50)/10d;
             }
         }
     }
 
-    int evaluate(float[] inputs){
-        if(inputs.Length != numInputs){
+    public int evaluate(float[] inputs){
+        if(inputs.Length != this.numInputs){
             return -1;
         }
+        layer1Vals = new double[this.layerSize];
+        layer2Vals = new double[this.layerSize];
+        layer3Vals = new double[this.layerSize];
+        actionVals = new double[this.numOutputOptions];
 
-        double[] input = new double[this.numInputs];
-        double[,] layer1 = new double[this.numInputs*4,this.numOutputOptions*4];
-        double[,] layer2 = new double[this.numOutputOptions*4, this.numOutputOptions];
-        double[] action = new double[this.numOutputOptions];
-
-        for(int i = 0;i<this.numInputs;i++){
-            input[i] = inputs[i]*inputWeights[i];
+        //Input Eval
+        for(int i = 0;i<this.layerSize;i++){
+            layer1Vals[i] = this.layer1Neurons[i].bias;
+            for(int j = 0; j<this.numInputs; j++) layer1Vals[i] += inputs[j]*this.layer1Neurons[i].weights[j] ;
         }
-        for(int i = 0;i<this.numInputs*4;i++){
-            for(int j=0;j<this.numOutputOptions*4;j++){
 
-            }
+        //Hidden Layer 1 eval
+        for(int i = 0;i<this.layerSize;i++){
+            layer2Vals[i] = this.layer1Neurons[i].bias;
+            for(int j = 0; j<this.layerSize; j++) layer2Vals[i] += layer1Vals[j]*this.layer2Neurons[i].weights[j] ;
         }
+
+        //Hidden Layer 2 eval
+        for(int i = 0;i<this.layerSize;i++){
+            layer3Vals[i] = this.layer3Neurons[i].bias;
+            for(int j = 0; j<this.layerSize; j++) layer3Vals[i] += layer2Vals[j]*this.layer3Neurons[i].weights[j] ;
+        }
+
+        //Output Layer eval
+        int max_index = 0;
+        for(int i = 0;i<this.numOutputOptions;i++){
+            actionVals[i] = this.actionNeurons[i].bias;
+            for(int j = 0; j<this.layerSize; j++) actionVals[i] += layer3Vals[j]*this.actionNeurons[i].weights[j];
+            if(actionVals[i] >= actionVals[max_index]) max_index = i;
+        }
+        // Debug.Log(action[0] + " " +action[1] + " " +action[2] + " " );
 
         // Go through the rest of the layers, applying weights to input from previous layer
         // return index of max value from action option layer
-        return -1;
+        return max_index;
     }
 
-    void train(int reward){
-        //strongest and weakest 20% of the final weights are adjusted by adding or subtracting some multiple of the reward
+    public void train(int reward, float [] inputs){
+        //strongest and weakest 20% of the final weights are adjusted by adding or
+        //  subtracting some multiple of the reward
         //Recurse this process back through the layers until reaching the input layer.
+
+
+        /* MATH TIME!
+            All the weight functions are linear in a multivariable space. So their gradients are always just one value per variable.
+            The wieght functinos must be differentiated with respect to the weight, however, so the values are really just the input values.
+            Here is the generic weight approximation function:
+
+            w^ = w^ + trainingCoefficient(reward + discountRate(maximum reward value for all neurons added together in current state) - (actual nuerons added up)*gradient)
+            
+            This evaluation should move the various weights up or down relative to the reward because it and the weights are capable of being negative
+            The same thing will be applied to the bias.
+        */
+
+        //layer1 training
+        for(int i = 0;i<this.layerSize;i++){
+            for(int j = 0; j<this.numInputs; j++) this.layer1Neurons[i].weights[j] = this.layer1Neurons[i].weights[j]  + trainingCoefficient(re);
+            this.layer1Neurons[i].bias = this.layer1Neurons[i].bias * (1+reward*trainingCoefficient);
+        }
+
+        //layer 2 traning
+        for(int i = 0;i<this.layerSize;i++){
+            for(int j = 0; j<this.layerSize; j++) this.layer2Neurons[i].weights[j] = this.layer2Neurons[i].weights[j] * (1+reward*trainingCoefficient);
+            this.layer2Neurons[i].bias = this.layer2Neurons[i].bias * (1+reward*trainingCoefficient);
+        }
+
+        //layer 3 training
+        for(int i = 0;i<this.layerSize;i++){
+            for(int j = 0; j<this.layerSize; j++) this.layer3Neurons[i].weights[j] = this.layer3Neurons[i].weights[j] * (1+reward*trainingCoefficient);
+            this.layer3Neurons[i].bias = this.layer3Neurons[i].bias * (1+reward*trainingCoefficient);
+        }
+
+        //Output Layer training
+        for(int i = 0;i<this.numOutputOptions;i++){
+            for(int j = 0; j<this.layerSize; j++) this.actionNeurons[i].weights[j] = this.actionNeurons[i].weights[j] * (1+reward*trainingCoefficient);
+            this.actionNeurons[i].bias = this.actionNeurons[i].bias * (1+reward*trainingCoefficient);
+        }
     }
 }
